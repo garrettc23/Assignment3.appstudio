f7fe20e71de82e5000576076dafbf56fdb95514a
garrettc23 <69862576+garrettc23@users.noreply.github.com>
2021-09-30 13:18:26 -0500

    Update

---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,6 @@
+/*
+
+let products = ['Crocheted Dog Toy', 'Slow Feeder Bowl', 'Pawz Grey Treat Jar', 'Blue Sweet Dreams Thermal Pajamas', 'Checker Chewy Vuiton Bowl', 'Red Heart Sweater Dress', 'Velvet Hair Bow', 'Into the Wild Leash']
+let price = ['$10.00', '$14.00', '$20.00', '$30.00', '$32.00', '$27.00', '$12.00', '$27.00']
+
+*/
